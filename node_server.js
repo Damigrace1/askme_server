e7014be218a9ab94fb7e5aba97daed558add7f0d
unknown <ogdamtim@gmail.com>
2023-05-31 14:57:58 +0100

    connect with mongo db

--- node_server.js
+++ node_server.js
@@ -1,42 +1,43 @@
 const express = require('express');
-const fs = require('fs');
+const User = require('./models/users');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3030;
-const dbURI = 'mongodb+srv://dami:icui4cu2@cluster0.qitd9ao.mongodb.net/Firstserverdb?retryWrites=true&w=majority'
-mongoose.connect(dbURI).then((res)=>{
-    console.log('connected' + res);
-    app.listen(PORT, () => {
-        console.log(`server started on port ${PORT}`);
-      });
-    }).catch((err) => console.log(e));
-
-app.use(function (req, res, next) {
-    console.log(req.body);
-    next();
-});
-   
-app.post('/api/test',(req,res)=>{
-    
-    const reqParam = req.body;
-    res.send(
-        {
-            "statusCode" : 200,
-            "title":"successs"
-        }
-    );
-   
-});
+const dbURI = 'mongodb+srv://dami:icui4cu2@cluster0.qitd9ao.mongodb.net/userdetails?retryWrites=true&w=majority'
+mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true }).then((res)=>{
+console.log('connected' + res);
+app.listen(PORT,() => {
+    console.log(`server started on port`);
+  });
+}).catch((err) => console.log('error' + err));
 
 app.get(
     '/',(req,res)=>{
-        console.log('dfghj');
-        fs.readFile('./sec.html',(err,data) =>{
-            if(!err){
-                res.setHeader('Content-Type','text/html');
-                res.write(data);
-                res.end();
-            }
+        const user = new User({
+          title:'Dami',
+          age:'24'  
         });
+        user.save()
+        .then((result) =>{
+            res.send(result);
+        }).catch((err)=>{
+            console.log(err);
+        });
+    }
+)
+app.post(
+    '/reg',(req,res)=>{
+        console.log(req);
+        
+        // const user = new User({
+        //   title:req.body['title'],
+        //   age:req.body['age']  
+        // });
+        // user.save()
+        // .then((result) =>{
+        //     res.send("Reg Success!");
+        // }).catch((err)=>{
+        //     console.log(err);
+        // });
     }
 )
