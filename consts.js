936ea19204300676370c17576058dae6cb12f3a6
unknown <ogdamtim@gmail.com>
2023-06-02 22:33:11 +0100

    do reg user api and others

--- consts.js
+++ consts.js
@@ -1,6 +1,6 @@
 
-const dbURI = 'mongodb+srv://' + 'dami' + ':' 
-+ 'icui4cu2'+ '@cluster0.qitd9ao.mongodb.net/userdetails?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://' + process.env.USER + ':' 
++ process.env.PW+ '@cluster0.qitd9ao.mongodb.net/userdetails?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 3030;
 module.exports = {
   dbURI , PORT
